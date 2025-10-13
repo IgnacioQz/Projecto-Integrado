cf5925e7268f80220c6f074bd2022fceef983786
IgnacioQz <nacho.quierozepeda@gmail.com>
2025-10-13 12:40:28 -0300

    pagina welcome lista

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.welcome, name='welcome'),
     path('login/', views.login, name='login'),
     path('main/', views.main, name='main'),
 ]
